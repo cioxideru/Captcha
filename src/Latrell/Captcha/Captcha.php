23841f94c923228eda724a339c5ae8fa50f36bbd
ibaranov <cioxideru@gmail.com>
2014-12-04 15:27:39 +0300

    fix

--- src/Latrell/Captcha/Captcha.php
+++ src/Latrell/Captcha/Captcha.php
@@ -96,7 +96,7 @@ class Captcha
     public static function check($value)
     {
         $captcha_hash = (string) Session::get('captcha_hash');
-        Session::forget('captcha_hash');
+        //Session::forget('captcha_hash');
         return $captcha_hash && Hash::check(Str::lower($value), $captcha_hash);
     }
 
